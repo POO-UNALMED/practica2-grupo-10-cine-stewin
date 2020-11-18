6e46ffb63c5d665c18484754c9fc618a7cbf0464
kmolano <kmolano@gmail.com>
2020-11-18 17:59:57 -0500

    Pantalla de inicio a la mitad

--- src/InterfasGrafica/interfasGrafica.java
+++ src/InterfasGrafica/interfasGrafica.java
@@ -1,34 +1,146 @@
 package InterfasGrafica;
 
-import InterfasGrafica.Ventanas.VentanaInicial;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.FileInputStream;
+import java.util.Vector;
+
 
 public class interfasGrafica extends Application{
-
-
+    Image imagenes;
+    int contadorCines = 0;
 
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Cine Stewin");
-        primaryStage.setScene(new VentanaInicial().getEscenaSaludo());
+        Vector<ImageView> cinesVect = new Vector<ImageView>(); /*arreglo donde se guardaran los cines*/
+        Vector<ImageView> autoresVect = new Vector<ImageView>(); /*Imagenes de nosotros*/
+
+
+        //Definimos la pantalla de inicio
+        FlowPane fondoPrincipal = new FlowPane();
+
+        //Los elementos en esta tendran un separado de 5px
+        fondoPrincipal.setVgap(0);
+        fondoPrincipal.setHgap(0);
+
+        //Definimos el panel izquierdo
+        GridPane izquierda = new GridPane();
+
+        //Definimos el panel derecho
+        GridPane derecha = new GridPane();
+
+        //Estas definimos las tres subregiones que tendra cada region(Izquierda derecha)
+        Label unoIzquierda = new Label();
+        Label dosIzquierda = new Label();
+        Label tresIzquierda = new Label("Aca debe ir el espacio para ingresar");
+        tresIzquierda.setMinWidth(350);
+        tresIzquierda.setMinHeight(300);
+        tresIzquierda.setStyle("-fx-border-color: red");
+        Label unoDerecha = new Label("Nuestros datos");
+        Label dosDerecha = new Label("");
+
+        //Aca ponemos cada una de los subregiones en su respectivo espacio
+        izquierda.add(unoIzquierda,0,0);
+        izquierda.add(dosIzquierda,0,1);
+        izquierda.add(tresIzquierda,0,2);
+        izquierda.setAlignment(Pos.CENTER_LEFT);
+        derecha.add(unoDerecha,0,0);
+        derecha.add(dosDerecha,0,1);
+        derecha.setAlignment(Pos.BASELINE_RIGHT);
+
+        //Ponemos el gridPane izquierdo y dercho en su lugar correspondondiente
+        fondoPrincipal.getChildren().add(izquierda);
+        fondoPrincipal.getChildren().add(derecha);
+
+        //RegionUno Izquierda
+        //Conseguimos la imagenen de Bienvenida
+        try{
+            imagenes = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/InterfasGrafica/Imagenes/Hola.png"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        ImageView imagenArreglada = new ImageView(imagenes);
+        //Podemos la imagen de Bienvenida en la parte superior del panel izquierdo
+        unoIzquierda.setGraphic(imagenArreglada);
+
+        //Centramos para que no se descuadre cuando se amplia
+        fondoPrincipal.setAlignment(Pos.CENTER);
+
+
+        //RegiosDos Izquierda
+        //Debajo de la imagen de Bienvenida debemos tener una lista de imagenes que se cambia cada que dan clic sobre ella
+        try{
+            for(int i=0; i<5;i++){
+                imagenes = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/InterfasGrafica/Imagenes/cine/"+i+".png"));
+                ImageView cines = new ImageView(imagenes);
+                cinesVect.add(cines);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        //Ponemos por defecto la primera imagen
+        dosIzquierda.setGraphic(cinesVect.get(0));
+        //Luego, cada que un usuario saque su mouse de la region, esta cambiara de imagen
+        dosIzquierda.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                contadorCines++;
+                dosIzquierda.setGraphic(cinesVect.get(contadorCines%5));
+            }
+        });
+
+        //RegionUno Derecha (Aun no esta, no se como :V)
+        unoDerecha.setMinWidth(350);
+        unoDerecha.setMinHeight(200);
+        unoDerecha.setStyle("-fx-border-color: red");
+
+        //RegionDos Derecha (Aca deben ir nuestras imagenes) 500px de profundo
+        //En esta parte deben ir imagenes con nuestras fotos y eso, pero por ahora no tengo tiempo de editarlas
+        //El tamaño por si alguien quiere el 350x500px
+
+        //Conseguimos todas las imagenes de nosotros
+        try{
+            for(int i=0; i<4;i++){
+                imagenes = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/InterfasGrafica/Imagenes/autores/"+i+".png"));
+                ImageView autores = new ImageView(imagenes);
+                //Las guardamos en este vector
+                autoresVect.add(autores);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        //Siempre por defecto mostramos la imagen 0
+        dosDerecha.setGraphic(autoresVect.get(0));
+
+        dosDerecha.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                contadorCines++;
+                dosDerecha.setGraphic(autoresVect.get((contadorCines%4)));
+            }
+        });
+
+
+
+        //Le damos el titulo a la escena
+        primaryStage.setTitle("Casa");
+
+        //Le pasamos la pantalla de inicio y el tamaño por defecto
+        primaryStage.setScene(new Scene(fondoPrincipal,700,700));
+
+        //Mostramos
         primaryStage.show();
     }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-    
- 
-    
 }
