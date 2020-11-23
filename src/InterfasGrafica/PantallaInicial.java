39ce92eea89c6f86c9ced06fad97fef92bcfd46d
kmolano <kmolano@gmail.com>
2020-11-23 01:58:49 -0500

    Puliendo 1.062

--- src/InterfasGrafica/PantallaInicial.java
+++ src/InterfasGrafica/PantallaInicial.java
@@ -24,24 +24,25 @@ import javafx.stage.Stage;
 import java.io.FileInputStream;
 import java.util.Vector;
 
-public class PantallaInicial extends Application{
-    Image imagenes;
-    Vector<String> datosAutores = new Vector<String>(); /*Tendremos los autores aca */
-    int contadorCines = 0;
-    int contadorAutores = 0;
-    Empleado empleado = new Empleado();
+public class PantallaInicial extends Application {
+    public Image imagenes;
+    public Vector<String> datosAutores = new Vector<String>(); /*Tendremos los autores aca */
+    public int contadorCines = 0;
+    public int contadorAutores = 0;
+    public Empleado empleado = new Empleado();
 
-    //Metodo que se ejecuta al principio para leer la base de datos
-    public void init(){
+    /*Metodo que se ejecuta al principio para leer la base de datos y relacionar los datos*/
+    public void init() {
         Leer.Leer();
         BaseDeDatos.relacionar();
-        System.out.println(BaseDeDatos.getClientes().size()); //--------------------------------
+        System.out.println(BaseDeDatos.getClientes().size());
         empleado.enlazarFuncionesYCines();
         empleado.definirSemanaFunciones();
     }
+
     public void start(Stage primaryStage) throws Exception {
 
-        //Creacion de diferentes componentes
+        /*Creacion de diferentes componentes*/
         String autor0 = ("Desarrolladores \nSepulveda Daniel Alejandro\ndasepulvedao@unal.edu.co");
         String autor1 = ("Desarrolladores \nOspina Amilder Stewin\naospinato@unal.edu.co");
         String autor2 = ("Desarrolladores \nMoreno Gelier Esteban\ngemorenog@unal.edu.co");
@@ -50,157 +51,162 @@ public class PantallaInicial extends Application{
         datosAutores.add(autor1);
         datosAutores.add(autor2);
         datosAutores.add(autor3);
-        Vector<ImageView> cinesVect = new Vector<ImageView>(); /*arreglo donde se guardaran los cines*/
+        Vector<ImageView> cinesVect = new Vector<ImageView>(); /*Arreglo donde se guardaran los cines*/
         Vector<ImageView> autoresVect = new Vector<ImageView>(); /*Imagenes de nosotros*/
 
-        //Definimos la pantalla de inicio
+        /*Definimos la pantalla de inicio*/
         FlowPane fondoPrincipal = new FlowPane();
 
-        //Los elementos en esta tendran un separado de 5px
-        fondoPrincipal.setVgap(0);
-        fondoPrincipal.setHgap(0);
-
-        //Definimos el panel izquierdo
+        /*Definimos el panel izquierdo*/
         GridPane izquierda = new GridPane();
 
-        //Definimos el panel derecho
+        /*Definimos el panel derecho*/
         GridPane derecha = new GridPane();
 
-        //Estas definimos las tres subregiones que tendra cada region(Izquierda derecha)
+        /*Definimos las tres subregiones que tendra la parte Izquierda*/
         Label unoIzquierda = new Label();
         Label dosIzquierda = new Label();
 
-        //La parte tresIzquierda debe ser un GridPane para poder organizar todos los objetos
+        /*La parte tresIzquierda debe ser un GridPane para poder organizar todos los objetos (Botones)*/
         GridPane tresIzquierda1 = new GridPane();
 
-        //tresIzquierda.setMinWidth(350);
-        //tresIzquierda.setMinHeight(300);
+        /*Definomos el tamaño de esta sub region*/
         tresIzquierda1.setMinHeight(350);
         tresIzquierda1.setMinHeight(300);
-        //tresIzquierda.setStyle("-fx-border-color: red");
-        tresIzquierda1.setStyle("-fx-border-color: red");
-        GridPane unoDerecha = new GridPane ();
-        //Este label sera el encargado de mostrar nuestra informacion
+
+        /*Definimos las dos sub regiones que tendra la parte Derecha*/
+        GridPane unoDerecha = new GridPane();
+
+        /*Este label sera el encargado de mostrar nuestra informacion*/
         Label textoAutores = new Label(datosAutores.get(0));
         Label dosDerecha = new Label("");
 
-        //Aca ponemos cada una de los subregiones en su respectivo espacio
-        izquierda.add(unoIzquierda,0,0);
-        izquierda.add(dosIzquierda,0,1);
-        izquierda.add(tresIzquierda1,0,2);
+        /*Aca ponemos cada una de los sub regiones en su respectivo espacio y las centramos*/
+        izquierda.add(unoIzquierda, 0, 0);
+        izquierda.add(dosIzquierda, 0, 1);
+        izquierda.add(tresIzquierda1, 0, 2);
         izquierda.setAlignment(Pos.CENTER_LEFT);
-        derecha.add(unoDerecha,0,0);
-        derecha.add(dosDerecha,0,1);
+        derecha.add(unoDerecha, 0, 0);
+        derecha.add(dosDerecha, 0, 1);
         derecha.setAlignment(Pos.BASELINE_RIGHT);
 
-        //Ponemos el gridPane izquierdo y dercho en su lugar correspondondiente
+        /*Ponemos el gridPane izquierdo y dercho en su lugar correspondondiente definimos el tamaño de la ventana*/
         fondoPrincipal.getChildren().add(izquierda);
         fondoPrincipal.getChildren().add(derecha);
         fondoPrincipal.setMinWidth(700);
         fondoPrincipal.setMinHeight(700);
 
-        //RegionUno Izquierda
+        /*--------------------RegionUno Izquierda--------------------*/
+
         //Conseguimos la imagenen de Bienvenida
-        try{
+        try {
             imagenes = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/InterfasGrafica/Imagenes/Hola.png"));
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         ImageView imagenArreglada = new ImageView(imagenes);
+        /*En el entregable decia un saludo de bienvenida, no especifico de que tipo, nos parecio mas bonito poner
+          una imagen :)*/
         //Podemos la imagen de Bienvenida en la parte superior del panel izquierdo
         unoIzquierda.setGraphic(imagenArreglada);
 
         //Centramos para que no se descuadre cuando se amplia
         fondoPrincipal.setAlignment(Pos.CENTER);
 
-        //RegiosDos Izquierda
-        //Debajo de la imagen de Bienvenida debemos tener una lista de imagenes que se cambia cada que dan clic sobre ella
-        try{
-            for(int i=0; i<5;i++){
-                imagenes = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/InterfasGrafica/Imagenes/cine/"+i+".png"));
+        /*--------------------RegiosDos Izquierda--------------------*/
+        /*Debajo de la imagen de Bienvenida debemos tener una lista de imagenes que se cambia cada que dan clic sobre ella*/
+
+        try {
+            /*Leemos cada una de las imagenes que tenemos y las guardamos en un vector para su posterior uso*/
+            for (int i = 0; i < 5; i++) {
+                imagenes = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/InterfasGrafica/Imagenes/cine/" + i + ".png"));
                 ImageView cines = new ImageView(imagenes);
                 cinesVect.add(cines);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        //Ponemos por defecto la primera imagen
+        /*Ponemos por defecto la primera imagen*/
         dosIzquierda.setGraphic(cinesVect.get(0));
+
         //Luego, cada que un usuario saque su mouse de la region, esta cambiara de imagen
         dosIzquierda.setOnMouseExited(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 contadorCines++;
-                dosIzquierda.setGraphic(cinesVect.get(contadorCines%5));
+                dosIzquierda.setGraphic(cinesVect.get(contadorCines % 5));
             }
         });
 
-        //RegionUno Derecha (Aun no esta, no se como :V)
+        /*RegionUno Derecha (Aun no esta, no se como :V)*/
         unoDerecha.setMinWidth(350);
         unoDerecha.setMinHeight(200);
         unoDerecha.setStyle("-fx-border-color: red");
         unoDerecha.setStyle("-fx-background-color: BLACK");
 
-        //RegionDos Derecha (Aca deben ir nuestras imagenes)
-        //Conseguimos todas las imagenes de nosotros
-        try{
-            for(int i=0; i<4;i++){
-                imagenes = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/InterfasGrafica/Imagenes/autores/"+i+".png"));
+        /*--------------------RegionDos Derecha--------------------*/
+        /*Conseguimos todas las imagenes de nosotros*/
+        try {
+            for (int i = 0; i < 4; i++) {
+                /*Leemos cada una de nuestras imagenes y las guardamos en un vector*/
+                imagenes = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/InterfasGrafica/Imagenes/autores/" + i + ".png"));
                 ImageView autores = new ImageView(imagenes);
                 //Las guardamos en este vector
                 autoresVect.add(autores);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        //Siempre por defecto mostramos la imagen 0
+        /*Siempre por defecto mostramos la imagen 0*/
         dosDerecha.setGraphic(autoresVect.get(0));
+
+        /*Evento que permite cambiar de imagen y te hoja de vida cada que alguien le da clic*/
         dosDerecha.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 contadorAutores++;
-                dosDerecha.setGraphic(autoresVect.get((contadorAutores%4)));
+                dosDerecha.setGraphic(autoresVect.get((contadorAutores % 4)));
                 unoDerecha.getChildren().remove(0);
-                textoAutores.setText(datosAutores.get(contadorAutores%4));
+                textoAutores.setText(datosAutores.get(contadorAutores % 4));
                 unoDerecha.getChildren().add(textoAutores);
-                dosDerecha.setGraphic(autoresVect.get((contadorAutores%4)));
+                dosDerecha.setGraphic(autoresVect.get((contadorAutores % 4)));
             }
         });
 
-        //Agregamos los dos botones que iran en la parte tres de la izquierda
-        //Boton para registrarse
+        /*--------------------RegionTres Izquierdda--------------------*/
+        /*Boton para registrarse*/
         Button registrar = new Button("Registrarse");
 
-        //Boton para entrar
+        /*Boton para entrar*/
         Button entrar = new Button("Entrar");
 
-        //Centramos los botones
+        /*Centramos los botones*/
         tresIzquierda1.setAlignment(Pos.CENTER);
         tresIzquierda1.setVgap(10);
         tresIzquierda1.setHgap(10);
 
-        //Definimos el tamano del boton registrar
+        /*Definimos el tamano del boton registrar*/
         registrar.setMinWidth(100);
         registrar.setMinHeight(50);
 
-        //Definimos el tamano del boton entrar
+        /*Definimos el tamano del boton entrar*/
         entrar.setMinWidth(100);
         entrar.setMinHeight(50);
 
-        //Ponemos los botones en su respectiva posiciion
-        tresIzquierda1.add(entrar,0,0);
-        tresIzquierda1.add(registrar,0,1);
-        //Podemos la parte tres con fondo negro
+        /*/Ponemos los botones en su respectiva posicion*/
+        tresIzquierda1.add(entrar, 0, 0);
+        tresIzquierda1.add(registrar, 0, 1);
+        /*Podemos la parte tres con fondo negro*/
         tresIzquierda1.setStyle("-fx-background-color: BLACK");
 
-        //Evento que ocurre a la hora de registrar un usuario
+        /*Evento que ocurre a la hora de registrar un usuario*/
         registrar.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
+                /*Ejecutamos una ventana para poder registrarme*/
                 CrearMenuRegistro.showing("Registro");
-
             }
         });
 
@@ -208,47 +214,50 @@ public class PantallaInicial extends Application{
         entrar.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
+                /*Ejecutamos que me pide mis datos para poder ingresar*/
                 Ingreso.showing(primaryStage);
             }
         });
-        textoAutores.setTextFill(Color.WHITE);
-        textoAutores.setFont(new Font("Arial Black",20));
-        textoAutores.setEffect(new Glow(1.7f));
 
-        //Area uno a la derecha
+        /*--------------------RegionUno Derecha--------------------*/
+
+        /*Definimos estilo y agregamos el nombre de la primera persona*/
+        textoAutores.setTextFill(Color.WHITE);
+        textoAutores.setFont(new Font("Arial Black", 20));
+        textoAutores.setEffect(new Glow(1.7f));
         unoDerecha.getChildren().add((textoAutores));
         unoDerecha.setAlignment(Pos.CENTER);
 
-        //Evento que ocurre cuando le dan clic en nuestras hojas de vida
+        /*Evento que ocurre cuando le dan clic en nuestras hojas de vida*/
         textoAutores.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 contadorAutores++;
                 unoDerecha.getChildren().remove(0);
-                textoAutores.setText(datosAutores.get(contadorAutores%4));
+                textoAutores.setText(datosAutores.get(contadorAutores % 4));
                 unoDerecha.getChildren().add(textoAutores);
-                dosDerecha.setGraphic(autoresVect.get((contadorAutores%4)));
+                dosDerecha.setGraphic(autoresVect.get((contadorAutores % 4)));
             }
         });
 
 
-        //Le damos el titulo a la escena
+        /*Le damos el titulo a la escena*/
         primaryStage.setTitle("Cine Stewin");
 
-        //Le pasamos la pantalla de inicio y el tamaño por defecto
-        primaryStage.setScene(new Scene(fondoPrincipal,700,700));
+        /*Le pasamos la pantalla de inicio y el tamaño por defecto*/
+        primaryStage.setScene(new Scene(fondoPrincipal, 700, 700));
 
         /*De esta forma el tamano minimo de la ventana sera este, asi no se podra deformar
-          Si el profesor la pone mas pequeña :v */
+          por si el profesor la pone mas pequena xd */
         primaryStage.setMinWidth(716);
         primaryStage.setMinHeight(740);
 
-        //Mostramos
+        /*Mostramos*/
         primaryStage.show();
     }
 
-    //Metodo que se ejecuta al principio para guardar los datos en la BaseDeDatos
-    public void stop(){
+    /*Metodo que se ejecuta al final para guardar los datos en la BaseDeDatos*/
+    public void stop() {
         Escribir.Escribir();
     }
 
